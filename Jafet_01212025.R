41752fa0405cd6ce2dd46c0846eecb4fe5053aaa
Jafetus <jrivera4@wvstateu.edu>
2025-01-21 18:50:17 -0500

    Add files via upload

---
+++ Jafet_01212025.R
@@ -0,0 +1,30 @@
+"HELL"
+2+9
+HELL
+Data = c(1,2,3)
+data
+view(Data)
+view (Data)
+jafetsdataframe <- data.frame(
+  name = c("lee", "pete"),
+  age = c(21, 34),
+  height = c(176, 185)
+)
+jafetsdataframe
+attach(jafetsdataframe)
+plot(age - height)
+getwd(
+)
+setwd("/Users/Student/Documents")
+library(ggplot)
+90982/20
+20^4
+Practicedataframe <- data.frame(
+  Name = c("Autumn", "Jafet", "Alan"),
+  Age = c(24, 23, 24),
+  Height = c(169, 178, 189)
+)
+attach(Practicedataframe)
+plot(Age - Height)
+plot(Height - Name)
+plot(Height)
